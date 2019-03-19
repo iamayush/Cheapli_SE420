50f4e05dd58959151e0e59e41496aa13784ccaf7
Sinha <ayush2@illinois.edu>
2019-03-19 09:25:22 -0500

    1st commit: works on single core w/ kalman filter

---
+++ Cheapli_SE420Project/Debug/configPkg/package/cfg/Cheapli_SE420_p28FP.cfg.mak
@@ -0,0 +1,8 @@
+# invoke SourceDir generated makefile for Cheapli_SE420.p28FP
+Cheapli_SE420.p28FP: .libraries,Cheapli_SE420.p28FP
+.libraries,Cheapli_SE420.p28FP: package/cfg/Cheapli_SE420_p28FP.xdl
+	$(MAKE) -f C:\ayush2\repo\trunk\Cheapli_SE420/src/makefile.libs
+
+clean::
+	$(MAKE) -f C:\ayush2\repo\trunk\Cheapli_SE420/src/makefile.libs clean
+
---
+++ Cheapli_SE420Project/Debug/configPkg/package/cfg/Cheapli_SE420_p28FP.h
@@ -0,0 +1,49 @@
+/*
+ *  Do not modify this file; it is automatically 
+ *  generated and any modifications will be overwritten.
+ *
+ * @(#) xdc-B06
+ */
+
+#include <xdc/std.h>
+
+#include <ti/sysbios/family/c28/Hwi.h>
+extern const ti_sysbios_family_c28_Hwi_Handle hwi97_sciaTx;
+
+#include <ti/sysbios/family/c28/Hwi.h>
+extern const ti_sysbios_family_c28_Hwi_Handle hwi96_sciaRx;
+
+#include <ti/sysbios/family/c28/Hwi.h>
+extern const ti_sysbios_family_c28_Hwi_Handle hwi92_scicRx;
+
+#include <ti/sysbios/family/c28/Hwi.h>
+extern const ti_sysbios_family_c28_Hwi_Handle hwi93_scicTx;
+
+#include <ti/sysbios/family/c28/Hwi.h>
+extern const ti_sysbios_family_c28_Hwi_Handle hwi99_scibTx;
+
+#include <ti/sysbios/family/c28/Hwi.h>
+extern const ti_sysbios_family_c28_Hwi_Handle hwi98_scibRx;
+
+#include <ti/sysbios/knl/Swi.h>
+extern const ti_sysbios_knl_Swi_Handle swi_print;
+
+#include <ti/sysbios/knl/Swi.h>
+extern const ti_sysbios_knl_Swi_Handle swi_balanceControl;
+
+#include <ti/sysbios/knl/Clock.h>
+extern const ti_sysbios_knl_Clock_Handle clock0;
+
+#include <ti/sysbios/family/c28/Hwi.h>
+extern const ti_sysbios_family_c28_Hwi_Handle hwi72_spia;
+
+#include <ti/sysbios/family/c28/Hwi.h>
+extern const ti_sysbios_family_c28_Hwi_Handle hwi74_spib;
+
+#include <ti/sysbios/family/c28/Hwi.h>
+extern const ti_sysbios_family_c28_Hwi_Handle hwi33_adc;
+
+extern int xdc_runtime_Startup__EXECFXN__C;
+
+extern int xdc_runtime_Startup__RESETFXN__C;
+
